06f82ac9dd2253c0ee3989a07175eb5657e2442b
nina higson <nina.higson@ao.com>
2024-01-16 11:00:15 +0000

    added more tests

--- BowlingTests/BowlingTests.cs
+++ BowlingTests/BowlingTests.cs
@@ -4,20 +4,11 @@ namespace BowlingTests
 {
     public class BowlingTests
     {
-		[TestCase("X X X X X X X X X X X X")]
-		public void GivenBowlingAlley_WhenPerfectGame_PlayerGets300Points(string scores)
-        {
-            var bowlingGame = new BowlingGame();
-            var listOfScores = bowlingGame.SplitScoresIntoFrames(scores);
-            var score = bowlingGame.GetScore(listOfScores);
-            Assert.That(score, Is.EqualTo(300));
-        }
-
 		[TestCase("9- 9- 9- 9- 9- 9- 9- 9- 9- 9-", 90)]
 		[TestCase("52 45 12 13 14 15 15 22 23 5-", 54)]
         [TestCase("X X X X X X X X X X X X", 300)]
         [TestCase("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5", 150)]
-        public void GivenBowlingAlley_WhenNoStrikesOrSpares_PlayerGetsCorrectPoints(string scores, int expectedScore)
+        public void GivenBowlingAlley_WhenProvidedAFullGameOfScores_PlayerGetsCorrectPoints(string scores, int expectedScore)
 		{
 			var bowlingGame = new BowlingGame();
 			var listOfScores = bowlingGame.SplitScoresIntoFrames(scores);
@@ -25,12 +16,15 @@ namespace BowlingTests
 			Assert.That(score, Is.EqualTo(expectedScore));
 		}
 
-		[TestCase("x")]
-		public void GivenBowlingAlley_WhenPlayerGetAStrike_Return10(string score)
+		[TestCase("x", 10)]
+        [TestCase("5/", 10)]
+        [TestCase("5-", 5)]
+        [TestCase("--", 0)]
+        public void GivenBowlingAlley_WhenPlayerGetAStrike_Return10(string score, int expectedRoundScore)
         {
             var bowlingGame = new BowlingGame();
             var roundScore = bowlingGame.CalculateScoreForRound(score);
-            Assert.That(roundScore, Is.EqualTo(10));
+            Assert.That(roundScore, Is.EqualTo(expectedRoundScore));
         }
 
         [TestCase("X X X X X X X X X X X X")]
